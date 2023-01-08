e220581657ae6b9473d5c22413990469fd6c334b
Avelino24 <119818860+Avelino24@users.noreply.github.com>
2023-01-08 19:15:59 +0000

    Add files via upload

---
+++ App.svelte
@@ -0,0 +1,47 @@
+<script>
+
+let src = "images/background.jpg";
+
+</script>
+
+
+<h1>ALBERT EINSTEIN</h1>
+
+<p>
+
+
+    Albert Einstein foi um físico e matemático alemão. Entrou para o rol dos maiores gênios da humanidade ao desenvolver a Teoria da Relatividade.
+
+    Estabeleceu a relação entre massa e energia e formulou a equação que se tornou a mais famosa do mundo: E = mc². Recebeu o Prêmio Nobel de Física, por suas descobertas sobre a lei dos efeitos fotoelétricos.
+
+
+
+</p>
+
+
+<style>
+
+h1{
+
+    color:rgba(218, 12, 12, 0.863);
+		font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+		font-size: large;
+     text-align: center;
+
+    
+}
+
+p{
+
+    border: solid rgb(255, 39, 39) 2px;
+    border-radius: 2px
+  
+
+}
+
+
+
+
+
+</style>
+
